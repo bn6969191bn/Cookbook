4f24d67fba47208e850162336553500c05374694
Bartosz Niziołek <34310@student.anstar.edu.pl>
2023-11-19 18:34:30 +0100

    exceptions handling and proper responses status

--- src/main/java/com/example/cookbook/service/serviceImpl/RecipeServiceImpl.java
+++ src/main/java/com/example/cookbook/service/serviceImpl/RecipeServiceImpl.java
@@ -5,10 +5,10 @@ import com.example.cookbook.model.Recipe;
 import com.example.cookbook.repository.RecipeRepository;
 import com.example.cookbook.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -22,50 +22,67 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public List<Recipe> getAllRecipes() {
-        return recipeRepository.findAll();
+        try {
+            return recipeRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving all recipes", e);
+        }
     }
 
     @Override
     public Recipe getRecipeById(String id) {
-        return recipeRepository.findById(id).orElse(null);
+        return recipeRepository.findById(id).orElseThrow(() -> new RecipeNotFoundException("Recipe with ID " + id + " not found"));
     }
 
     @Override
     public Recipe addRecipe(Recipe recipe) {
-        return recipeRepository.save(recipe);
+        if (recipe.getName() == null || recipe.getName().isEmpty() ||
+                recipe.getDescription() == null || recipe.getDescription().isEmpty() ||
+                recipe.getIngredients() == null || recipe.getIngredients().isEmpty() ||
+                recipe.getInstructions() == null || recipe.getInstructions().isEmpty()) {
+            throw new IllegalArgumentException("All fields (name, description, ingredients, instructions) are required");
+        }
+
+        try {
+            return recipeRepository.save(recipe);
+        } catch (Exception e) {
+            throw new RuntimeException("Error adding a new recipe", e);
+        }
     }
 
     @Override
     public void deleteRecipeById(String id) {
-        recipeRepository.deleteById(id);
+        try {
+            recipeRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RecipeNotFoundException("Recipe with ID " + id + " not found");
+        } catch (Exception e) {
+            throw new RuntimeException("Error deleting recipe with ID " + id, e);
+        }
     }
 
     @Override
     public Recipe updateRecipe(String id, Recipe updatedRecipe) {
-        Optional<Recipe> existingRecipeOptional = recipeRepository.findById(id);
+        Recipe existingRecipe = validateAndGetExistingRecipe(id);
 
-        if (existingRecipeOptional.isPresent()) {
-            Recipe existingRecipe = existingRecipeOptional.get();
-
-            if (updatedRecipe.getName() != null) {
-                existingRecipe.setName(updatedRecipe.getName());
-            }
-            if (updatedRecipe.getDescription() != null) {
-                existingRecipe.setDescription(updatedRecipe.getDescription());
-            }
-            if (updatedRecipe.getIngredients() != null) {
-                existingRecipe.setIngredients(updatedRecipe.getIngredients());
-            }
-            if (updatedRecipe.getInstructions() != null) {
-                existingRecipe.setInstructions(updatedRecipe.getInstructions());
-            }
-
-            return recipeRepository.save(existingRecipe);
+        if (updatedRecipe.getName() != null) {
+            existingRecipe.setName(updatedRecipe.getName());
+        }
+        if (updatedRecipe.getDescription() != null) {
+            existingRecipe.setDescription(updatedRecipe.getDescription());
+        }
+        if (updatedRecipe.getIngredients() != null) {
+            existingRecipe.setIngredients(updatedRecipe.getIngredients());
+        }
+        if (updatedRecipe.getInstructions() != null) {
+            existingRecipe.setInstructions(updatedRecipe.getInstructions());
         }
 
-        throw new RecipeNotFoundException("Recipe with ID " + id + " not found");
+        return recipeRepository.save(existingRecipe);
     }
 
-
+    private Recipe validateAndGetExistingRecipe(String id) {
+        return recipeRepository.findById(id)
+                .orElseThrow(() -> new RecipeNotFoundException("Recipe with ID " + id + " not found"));
+    }
 }
-
